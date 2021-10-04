312198fc4343ecabe25aef046606c6ed15e52e5d
halitbatur <halitfuatbatur@gmail.com>
2021-10-04 19:49:40 +0300

    delete the unsed view engines

--- app.js
+++ app.js
@@ -5,10 +5,6 @@ const port = 3001;
 const path = require("path");
 const bodyParser = require("body-parser");
 
-
-app.set("view engine", "ejs");
-app.set("views", "./views");
-
 app.use(express.static(path.join(__dirname, "public")));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
